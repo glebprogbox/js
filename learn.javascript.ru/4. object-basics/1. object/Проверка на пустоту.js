22472fd1275d84f31eb7cbcfe41eb4c515a87aa6
glebprogbox <glebprogbox08@mail.ru>
2026-02-17 00:32:50 +0300

    уроки

---
+++ learn.javascript.ru/4. object-basics/1. object/Проверка на пустоту.js
@@ -0,0 +1,10 @@
+isEmpty = (obj) => {
+    for (let prop in obj){ return false};
+    return true;
+    }
+
+
+let schedule = {};
+alert(isEmpty(schedule)); // true
+schedule["8:30"] = "get up";
+alert(isEmpty(schedule)); // false)
